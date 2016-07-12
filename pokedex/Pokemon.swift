6b3de12e71515eaad7395383d057f9001b9390d5
Ziga Strgar <ziga_strgar@hotmail.com>
2016-07-12 16:17:02 +0200

    Added Alamofire, finished detail

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -11,6 +11,12 @@ import Foundation
 class Pokemon {
     private var _name: String!
     private var _id: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionText: String!
     
     var name: String {
         return _name
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
+    @IBOutlet weak var nameLbl: UIView!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var nextEvoLbl: UIImageView!
+    @IBOutlet weak var curretnEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func backButtonTouched(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
