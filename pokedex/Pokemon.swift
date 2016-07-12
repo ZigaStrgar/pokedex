2f7f28d3cafe4be65ccad592401dc713ed702aaa
Ziga Strgar <ziga_strgar@hotmail.com>
2016-07-12 13:07:14 +0200

    Pokemon class

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Ziga Strgar on 12/07/16.
+//  Copyright © 2016 Ziga Strgar. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _id: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var id: Int {
+        return _id
+    }
+    
+    init(name: String, id: Int) {
+        _name = name
+        _id = id
+    }
+}
